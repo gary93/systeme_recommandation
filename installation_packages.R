beb11a89c88861bed524236f5a76c2eb307c57d4
gary93 <garyperez1993@gmail.com>
2018-01-15 23:43:09 +0100

    Mise à jour du fichier d'installation des packages

--- installation_packages.R
+++ installation_packages.R
@@ -2,4 +2,10 @@
 ## Download R packages.
 ##
 
-install.packages("jsonlite")
+packages = c("jsonlite", "dplyr", "recommenderlab", "data.table", "tidytext", "tidyr", "stringr", "gridExtra", "ggplot2")
+packages = lapply(packages, FUN = function(x) {
+    if(!require(x, character.only = TRUE)) {
+        install.packages(x)
+        library(x, character.only = TRUE)
+    }
+})
