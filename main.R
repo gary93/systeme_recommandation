8c1c222641c10e0bbcef4efb0fae33b8791cff67
jonassebaoni <jonassebaoni5@gmail.com>
2018-01-15 23:20:23 +0100

    Affichage des résultats obtenus dans graphique

--- main.R
+++ main.R
@@ -11,26 +11,26 @@ library(ggplot2)
 
 fichier_dataset = "dataset.json"
 
-# Importation du dataset
+print("Importation du dataset")
 donnees <- stream_in(file(fichier_dataset))
 
-# Récupération des 10000 premiers échantillons
+print("Récupération des 10000 premiers échantillons")
 echantillon_donnees <- donnees[10000,]
 
-# Analyse des critiques sentimentales
+print("Analyse des critiques sentimentales")
 echantillon_donnees$reviewText <- as.character(echantillon_donnees $reviewText)
 mots_revue <- echantillon_donnees %>%
     select(reviewerID, reviewText, overall) %>%
     unnest_tokens(word, reviewText) %>%
     filter(!word %in% stop_words$word, str_detect(word, "^[a-z']+$"))
 
-# Nrc
+print("-Nrc")
 nrc <- sentiments %>%
     filter(sentiment %in% c('positive','negative') & lexicon == 'nrc') %>%
     mutate(nrc = ifelse(sentiment == 'positive',1,-1)) %>%
     select(word, nrc)
 
-# Bing
+print("-Bing")
 bing <- sentiments %>%
     filter(lexicon == 'bing') %>%
     mutate(bing = ifelse(sentiment == 'positive',1,-1)) %>%
@@ -44,3 +44,16 @@ sommaire_scores_revues <- scores_revues %>%
     group_by(reviewerID, overall) %>%
     summarise(nrc_score = round(mean(nrc, na.rm = T),3),
     bing_score = round(mean(bing, na.rm = T),3))
+
+print("Affichage des résultats obtenus")
+nrc_graphique <- ggplot(sommaire_scores_revues, aes(x = as.character(overall), y = nrc_score))+
+    geom_boxplot()+
+    labs(x = 'Amazon',
+    y = 'NRC')
+
+bing_graphique <- ggplot(sommaire_scores_revues, aes(x = as.character(overall), y = bing_score))+
+    geom_boxplot()+
+    labs(x = 'Amazon',
+    y = 'Bing')
+
+grid.arrange(nrc_graphique, bing_graphique, nrow = 2)
